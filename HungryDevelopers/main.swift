66e4b6f4aa75682e3dc49ac706cb7a4de9b8bec9
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-20 14:22:05 -0600

    Initial Commit

---
+++ HungryDevelopers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HungryDevelopers
+//
+//  Created by Ilgar Ilyasov on 2/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
